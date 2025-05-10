70441cfa7b57027a419cf234ea2836f367fdb83c
Bronek Kozicki <brok@incorrekt.com>
2025-05-10 09:09:11 +0100

    Add RAII to PcapInputs

--- lib/pcap_inputs.cpp
+++ lib/pcap_inputs.cpp
@@ -2,36 +2,33 @@
 
 [[nodiscard]] auto PcapInputs::make(pair<std::string> filenames) -> std::expected<PcapInputs, error>
 {
-  // TODO: use RAII for fclose/pcap_close
-  pair<FILE *> files = {.A = std::fopen(filenames.A.c_str(), "rb"), .B = std::fopen(filenames.B.c_str(), "rb")};
+  struct file_closer final {
+    void operator()(FILE *f) const noexcept { std::fclose(f); }
+  };
+  using file_handle = std::unique_ptr<FILE, file_closer>;
+
+  pair<file_handle> files = {.A{std::fopen(filenames.A.c_str(), "rb")}, .B{std::fopen(filenames.B.c_str(), "rb")}};
   if (files.A == nullptr && files.B == nullptr) {
     return error::make("failed to open both files: ", filenames.A, ", ", filenames.B);
   }
   if (files.A == nullptr) {
-    std::fclose(files.B);
     return error::make("failed to open file A: ", filenames.A);
   }
   if (files.B == nullptr) {
-    std::fclose(files.A);
     return error::make("failed to open file B: ", filenames.B);
   }
 
   char buffer[PCAP_ERRBUF_SIZE + 1] = {};
-  pair<pcap_t *> ret = {};
+  pair<pcap_handle> ret = {.A{::pcap_fopen_offline(files.A.release(), buffer)}, .B{nullptr}};
   // https://www.tcpdump.org/manpages/pcap_open_offline.3pcap.html
-  ret.A = pcap_fopen_offline(files.A, buffer);
   if (ret.A == nullptr) {
-    std::fclose(files.A);
-    std::fclose(files.B);
     return error::make("invalid file A, error: ", buffer);
   }
 
-  ret.B = pcap_fopen_offline(files.B, buffer);
+  ret.B.reset(::pcap_fopen_offline(files.B.release(), buffer));
   if (ret.B == nullptr) {
-    pcap_close(ret.A);
-    std::fclose(files.B);
     return error::make("invalid file B, error: ", buffer);
   }
 
-  return PcapInputs(ret);
+  return PcapInputs(std::move(ret));
 }
--- lib/pcap_inputs.hpp
+++ lib/pcap_inputs.hpp
@@ -4,6 +4,8 @@
 #include "error.hpp"
 #include "inputs.hpp"
 
+#include <memory>
+
 #include <pcap.h>
 #include <pcap/pcap.h>
 
@@ -13,43 +15,39 @@ struct PcapInputs final : Inputs {
 
   // noncopyable, but moveable
   PcapInputs(PcapInputs const &) = delete;
-  PcapInputs(PcapInputs &&other) : A_(other.A_), B_(other.B_)
-  {
-    other.A_ = nullptr;
-    other.B_ = nullptr;
-  }
-
-  ~PcapInputs()
-  {
-    // https://www.tcpdump.org/manpages/pcap_close.3pcap.html
-    if (A_ != nullptr) {
-      pcap_close(A_);
-    }
-    if (B_ != nullptr) {
-      pcap_close(B_);
-    }
-  }
+  PcapInputs(PcapInputs &&other) : A_(std::move(other.A_)), B_(std::move(other.B_)) {}
 
 private:
-  explicit PcapInputs(pair<pcap_t *> src) : A_(src.A), B_(src.B) {}
+  struct pcap_closer final {
+    void operator()(pcap_t *p) const noexcept { ::pcap_close(p); }
+  };
+  using pcap_handle = std::unique_ptr<pcap_t, pcap_closer>;
+
+  explicit PcapInputs(pair<pcap_handle> src) : A_(std::move(src.A)), B_(std::move(src.B)) {}
 
   static auto next_(pcap_t *input, auto &&callback) -> bool
   {
     pcap_pkthdr *pkt_header = nullptr;
     unsigned char const *data = nullptr;
     // https://www.tcpdump.org/manpages/pcap_next_ex.3pcap.html
-    if (auto ret = pcap_next_ex(input, &pkt_header, &data); ret == 1) {
+    if (auto const ret = ::pcap_next_ex(input, &pkt_header, &data); ret == 1) {
       callback(pkt_header->caplen, pkt_header->len, data);
       return true;
     }
     return false;
   }
 
-  auto next_a(std::move_only_function<callback_t> callback) -> bool override { return next_(A_, std::move(callback)); }
-  auto next_b(std::move_only_function<callback_t> callback) -> bool override { return next_(B_, std::move(callback)); }
+  auto next_a(std::move_only_function<callback_t> callback) -> bool override
+  {
+    return next_(A_.get(), std::move(callback));
+  }
+  auto next_b(std::move_only_function<callback_t> callback) -> bool override
+  {
+    return next_(B_.get(), std::move(callback));
+  }
 
-  pcap_t *A_;
-  pcap_t *B_;
+  pcap_handle A_;
+  pcap_handle B_;
 };
 
 #endif // LIB_PCAP_INPUTS
